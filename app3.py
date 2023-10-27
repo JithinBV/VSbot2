d56f615327d3c1f0e1a878abada67027e48ac1d4
JithinBV <141017950+JithinBV@users.noreply.github.com>
2023-10-27 15:14:08 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -28,7 +28,7 @@ def main():
     if user_csv is not None:
         for file in user_csv:
             df_temp = pd.read_csv(file)
-            df_append = df_append.append(df_temp, ignore_index=True)
+            df_append.append(df_temp, ignore_index=True)
             df = pd.concat(df_append,ignore_index=True)
             llm = AzureOpenAI(deployment_name=AZURE_OPENAI_NAME, temperature=0)
             agent = create_pandas_dataframe_agent(llm,df_append,verbose=True)
