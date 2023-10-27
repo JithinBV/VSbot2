f3855cbb273c71721ccfe52fbff2246be911da86
JithinBV <141017950+JithinBV@users.noreply.github.com>
2023-10-27 15:20:25 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -27,6 +27,7 @@ def main():
     
     if user_csv is not None:
         for file in user_csv:
+            df_list[]
             data = pd.read_csv(file)
             df_list.append(data)
             df = pd.concat(df_list)
